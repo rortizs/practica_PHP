0268fc25e7febfa8310f74f1891a2dbaa55fc522
Richard david Ortiz sasvin <richarddavidortizsasvin@Richards-MacBook-Pro.local>
2022-10-16 16:05:23 -0600

    Practica PHP

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+//echo
+//echo ("Hola mundo");
+
+//variables
+$nombre = "Richard Ortiz";
+//echo $nombre;
+//var_dump($nombre);
+
+define('constante', 'este es el valor de la constante');
+//echo constante;
+
+const constante2 = "Hola mundo 2";
+//echo constante2;
+
+$nombreCliente = "Richard Ortiz Sasvin";
+$nombre_cliente = "David Ortiz";
+echo $nombre_cliente;
